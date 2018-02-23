b7b59d33b1205970bbb8dc69883b152bf405d32c
Andrew Smith <andrew@andrew.codes>
2018-02-23 11:45:50 -0500

    include npm script to bump all package versions at once

--- package-scripts.js
+++ package-scripts.js
@@ -3,5 +3,10 @@ module.exports = {
     default: 'nps',
     readme: 'lerna exec node ../../scripts/readme.js',
     publish: 'nps readme && lerna exec vsce publish',
+    version: {
+      major: 'lerna exec npm version minor',
+      minor: 'lerna exec npm version major',
+      patch: 'lerna exec npm version patch',
+    },
   },
 };
